bacad5ebc88227b70b304da8b8d5df5d284ce51b
Cedric <140487021+africaqs@users.noreply.github.com>
2023-08-01 00:39:58 +0200

    setting up Flask with hello wormd

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hello')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def give_me_this():
+  return 'Hello G'
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
